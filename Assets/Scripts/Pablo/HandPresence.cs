2598f07118ffc0d80482ef4967fc094e956e887a
Arryos <72743382+Arryos@users.noreply.github.com>
2021-04-18 18:55:50 +0200

    [+]main animation V1

--- Assets/Scripts/Pablo/HandPresence.cs
+++ Assets/Scripts/Pablo/HandPresence.cs
@@ -10,19 +10,62 @@ public class HandPresence : MonoBehaviour
 
     private GameObject spawnedHandModel;
     private InputDevice targetDevice;
+    private Animator HandAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
+        TryInitiatize();
+    }
+
+    private void TryInitiatize()
+    {
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(ControllerCharacteristics, devices);
 
-        spawnedHandModel = Instantiate(HandModelPrefab, transform);
-    }
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
 
+            spawnedHandModel = Instantiate(HandModelPrefab, transform);
+            HandAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+        
+    }
+       
     // Update is called once per frame
     void Update()
     {
-        
+        /*if (!targetDevice.isValid)
+        {
+            TryInitiatize();
+        }
+        else
+        {
+            UpdateHandAnimation();
+        }*/
+        UpdateHandAnimation();
+    }
+
+    private void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+
+            HandAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            //HandAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float GripValue))
+        {
+            HandAnimator.SetFloat("Grip", GripValue);
+        }
+        else
+        {
+            //HandAnimator.SetFloat("Grip", 0);
+        }
     }
 }
