fdb854d779f7512e579fb4bf606d7ff0f3343366
c-ledreau <c.ledreau@hotmail.fr>
2021-04-18 16:26:34 +0200

    [+] multiplayer v1

--- Assets/Scripts/Clement/networkManager.cs
+++ Assets/Scripts/Clement/networkManager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class networkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        ConnectToServer();
     }
 
     // Update is called once per frame
-    void Update()
+    void ConnectToServer()
     {
-        
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try connect to server ...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected to server");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("joined the room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("a new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
     }
 }
