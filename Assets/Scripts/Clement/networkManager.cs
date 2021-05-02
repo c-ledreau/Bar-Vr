255cd2852381099b5ab903f92be41c07e57d06ad
c-ledreau <c.ledreau@hotmail.fr>
2021-05-02 11:48:14 +0200

    [+] lobby

--- Assets/Scripts/Clement/networkManager.cs
+++ Assets/Scripts/Clement/networkManager.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
+[System.Serializable]
+public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
 public class networkManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        ConnectToServer();
-    }
 
+    public List<DefaultRoom> defaultRooms;
+    public GameObject roomUI;
     // Update is called once per frame
-    void ConnectToServer()
+    public void ConnectToServer()
     {
         PhotonNetwork.ConnectUsingSettings();
         Debug.Log("Try connect to server ...");
@@ -23,12 +28,30 @@ public class networkManager : MonoBehaviourPunCallbacks
     {
         Debug.Log("connected to server");
         base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+
+    public void InitializeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+        //load Scene
+        PhotonNetwork.LoadLevel(roomSettings.sceneIndex);
+
+        //create room
         RoomOptions roomOptions = new RoomOptions();
-        roomOptions.MaxPlayers = 10;
+        roomOptions.MaxPlayers = (byte)roomSettings.maxPlayer;
         roomOptions.IsVisible = true;
         roomOptions.IsOpen = true;
 
-        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom(roomSettings.Name, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("lobby joined");
+        roomUI.SetActive(true);
     }
 
     public override void OnJoinedRoom()
